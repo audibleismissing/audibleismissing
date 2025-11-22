fe302f36db2486a2f763701e7546f2e5996ad59c
Mike Tupker <mike@tupkers.net>
2025-11-22 12:47:12 -0600

    imported module

---
+++ app/app_helpers/audibleapi/api.py
@@ -0,0 +1,34 @@
+from typing import Any, Dict
+
+import audible
+
+from classes.custom_objects import Book
+
+
+
+# get book and return book object
+def getBook(auth, asin) -> Book:
+    from functions.audibleapi import returnBookObj
+    
+    with audible.Client(auth) as client:
+        item = client.get(
+            f"1.0/catalog/products/{asin}",
+            response_groups="product_desc, product_details, series, contributors, rating, category_ladders, relationships"
+        )
+        if item:
+            # print(json.dumps(item, indent=4)) # friendly json view
+            return returnBookObj(item)
+    return None
+
+
+def getAudibleBooksInSeries(auth, asin) -> Dict[str, Any]:
+    with audible.Client(auth) as client:
+        item = client.get(
+            f"/1.0/catalog/products/{asin}/sims",
+            response_groups="product_desc, product_details, series, contributors, rating",
+            similarity_type="InTheSameSeries",
+            num_results=50
+        )
+        if item:
+            return item
+    return None
