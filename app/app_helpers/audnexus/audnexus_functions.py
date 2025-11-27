d04b0d264d3c522c7eb15a1d6f64f141cc7b131d
Mike Tupker <tupcakes@users.noreply.github.com>
2025-11-27 12:35:37 -0600

    fixed typos.

--- app/app_helpers/audnexus/audnexus_functions.py
+++ app/app_helpers/audnexus/audnexus_functions.py
@@ -5,8 +5,10 @@ from app.db_models.tables.books import updateBook, getAllBooks, getBook
 from app.db_models.tables.series import addSeries, updateSeries, getSeries, cleanupDanglingSeries, calculateSeriesRating
 from app.db_models.tables.seriesmappings import addSeriesMapping
 from app.db_models.tables.authors import addAuthor, updateAuthor, getAuthor, cleanupDanglingAuthors
-from app.db_models.tables.genres import updateGenre, getGenre
-from app.db_models.tables.narrators import updateNarrator, getNarrator
+from app.db_models.tables.genres import addGenre, updateGenre, getGenre
+from app.db_models.tables.narrators import addNarrator, updateNarrator, getNarrator
+from app.db_models.tables.narratormappings import addNarratorMapping
+from app.db_models.tables.genremappings import addGenreMapping
 
 
 def backfillAudnexusBookData(engine) -> None:
@@ -51,13 +53,34 @@ def backfillAudnexusBookData(engine) -> None:
         if len(audnexus_book.authors) > 0:
             for single_authors in audnexus_book.authors:
                 if getAuthor(engine, single_authors.name):
-                    library_series = getAuthor(engine, single_authors.name)
-                    single_authors.id = library_series.id
+                    library_authors = getAuthor(engine, single_authors.name)
+                    single_authors.id = library_authors.id
                     updateAuthor(engine, single_authors)
                 else:
                     single_authors.id = addAuthor(engine, single_authors)
                     addAuthorMapping(engine, single_authors.id, audnexus_book.id)
 
+        # narrators
+        if len(audnexus_book.narrators) > 0:
+            for single_narrators in audnexus_book.narrators:
+                if getNarrator(engine, single_narrators.name):
+                    library_narrators = getNarrator(engine, single_narrators.name)
+                    single_narrators.id = library_narrators.id
+                    updateNarrator(engine, single_narrators)
+                else:
+                    single_narrators.id = addNarrator(engine, single_narrators)
+                    addNarratorMapping(engine, single_narrators.id, audnexus_book.id)
+                    
+        # genres
+        if len(audnexus_book.genres) > 0:
+            for single_genres in audnexus_book.genres:
+                if getGenre(engine, single_genres.name):
+                    library_genres = getGenre(engine, single_genres.name)
+                    single_genres.id = library_genres.id
+                    updateGenre(engine, single_genres)
+                else:
+                    single_genres.id = addGenre(engine, single_genres)
+                    addGenreMapping(engine, single_genres.id, audnexus_book.id)
 
     cleanupDanglingSeries(engine)
     cleanupDanglingAuthors(engine)
