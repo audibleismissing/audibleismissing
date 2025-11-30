4c1f6e92831f0b4235a354e52178ccc0606e7d76
Mike Tupker <mtupker@gmail.com>
2025-11-30 17:17:43 -0600

    better dev env

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ docker compose -f docker/compose-dev.yaml down
 # docker system prune -f
 docker builder prune -f
 docker rmi docker-audibleismissing-dev:latest
-docker compose -f docker/compose-dev.yaml up
+docker compose -f docker/compose-dev.yaml up --watch
 
 
 # docker exec -it audibleismissing-dev sh
