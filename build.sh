80d7c2a486813ba4d585b7fbc74156c6c4b89817
Mike Tupker <mtupker@gmail.com>
2025-12-13 11:08:06 -0600

    refactor build script to use buildx and ensure fresh builds

--- build.sh
+++ build.sh
@@ -2,9 +2,10 @@
 
 docker compose -f docker/compose-dev.yaml down
 # docker system prune -f
-docker builder prune -f
+docker buildx prune -f
 docker rmi docker-audibleismissing-dev:latest
-docker compose -f docker/compose-dev.yaml up --watch
+docker compose -f docker/compose-dev.yaml up --watch --build
+
 
 
 # docker exec -it audibleismissing-dev sh
